d0a02115cd8e44712b1097c44558dad9f6a13e0f
ekatrif <katiapervak@gmail.com>
2022-07-20 15:43:15 +0300

    added axios-downloading from json

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import "bootstrap/dist/css/bootstrap.css";
 
 import App from "./App.vue";
 
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+Vue.use(VueAxios, axios);
+
 Vue.config.productionTip = false;
 
 new Vue({
