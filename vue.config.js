35268acf3d18b99b82b571769b34087f86a2651a
ekatrif <katiapervak@gmail.com>
2022-07-25 14:49:39 +0300

    optimize images

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,28 @@
 const { defineConfig } = require("@vue/cli-service");
+
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const path = require("path");
+const imagePlugin = new ImageminPlugin({
+  disable: process.env.NODE_ENV !== "production", // Disable during development
+  pngquant: {
+    quality: "65-80",
+  },
+  jpegtran: {
+    // jpeg
+    quality: 65,
+  },
+  gifsicle: {
+    // gif
+    interlaced: true,
+    optimizationLevel: 3,
+  },
+  cacheFolder: path.join(__dirname, "./imageMinCache"),
+});
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "./",
+  configureWebpack: {
+    plugins: [imagePlugin],
+  },
 });
